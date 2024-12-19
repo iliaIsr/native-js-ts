d612d69f9f2b9cf436cf9129e83fbbb40791bff3
iliaIsr <ilil.sbl91@gmail.com>
2024-12-19 13:56:18 +0300

    lesson 11

--- src/11promis/Promis.js
+++ src/11promis/Promis.js
@@ -15,24 +15,60 @@ import axios from "axios";
 //     console.log(res[1].data)
 // })
 
-const ObjResp={
-    name:"Illia",
-    adress:{
-        city:"Minsk",
-        index:210024,
-        street:{
-            name:'Bagro',
-            house:23
-        }
-    }
-}
+// const ObjResp={
+//     name:"Illia",
+//     adress:{
+//         city:"Minsk",
+//         index:210024,
+//         street:{
+//             name:'Bagro',
+//             house:23
+//         }
+//     }
+// }
+//
+// async function run(){
+//     let adress=await ObjResp.adress;
+//     console.log(adress);
+//     let city=await adress.city;
+//     console.log(city);
+//     let name=await ObjResp.name;
+//     console.log(name);
+// }
+// run()
 
-async function run(){
-    let adress=await ObjResp.adress;
-    console.log(adress);
-    let city=await adress.city;
-    console.log(city);
-    let name=await ObjResp.name;
-    console.log(name);
-}
-run()
+//-----------------------------------
+// function getNumber(){
+//     const promise= new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve(Math.random()*1000);
+//         },1000)
+//     })
+//     return promise;
+// }
+// getNumber().then(res=>console.log(res))
+// getNumber().catch(r=>console.log(r))
+
+//-------------------------------------
+// const repo={
+//     save(data) {
+//
+//         localStorage.setItem("repo",JSON.stringify(data));
+//     }
+// }
+//
+// repo.save({name:'IT_Inkubator'});
+// console.log("saved")
+//-------------------------------------export const User=()=>{
+//
+    let counter=0
+    const Second=()=>{
+
+        setTimeout(()=>{
+            counter+=1;
+            console.log(counter)
+            Second()
+        },1000)
+
+    }
+    Second()
